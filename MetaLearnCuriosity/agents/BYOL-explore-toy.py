fd91713294122d9add168473e4bb669da30a4e89
Ziksby <ZKXBAT001@myuct.ac.za>
2023-10-01 12:13:36 +0200

    feat: :sparkles: add Transition class.

--- MetaLearnCuriosity/agents/BYOL-explore-toy.py
+++ MetaLearnCuriosity/agents/BYOL-explore-toy.py
@@ -8,4 +8,14 @@ from typing import Sequence, NamedTuple, Any
 from flax.training.train_state import TrainState
 import distrax
 import gymnax
-from Meta_learned_curiosity_jax.wrappers import LogWrapper, FlattenObservationWrapper
+from MetaLearnCuriosity.wrappers import LogWrapper, FlattenObservationWrapper
+
+
+class Transition(NamedTuple):
+    done: jnp.ndarray
+    action: jnp.ndarray
+    value: jnp.ndarray
+    reward: jnp.ndarray
+    log_prob: jnp.ndarray
+    obs: jnp.ndarray
+    info: jnp.ndarray
