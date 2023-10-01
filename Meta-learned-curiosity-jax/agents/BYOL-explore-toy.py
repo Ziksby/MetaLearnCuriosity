b5acd6fd37a7a75ab0a968aaa15a4197a7518700
Ziksby <ZKXBAT001@myuct.ac.za>
2023-10-01 10:38:49 +0200

    added imports to BYOL file.

---
+++ Meta-learned-curiosity-jax/agents/BYOL-explore-toy.py
@@ -0,0 +1,11 @@
+import jax
+import jax.numpy as jnp
+import flax.linen as nn
+import numpy as np
+import optax
+from flax.linen.initializers import constant, orthogonal
+from typing import Sequence, NamedTuple, Any
+from flax.training.train_state import TrainState
+import distrax
+import gymnax
+from Meta-learned-curiosity-jax.wrappers import LogWrapper, FlattenObservationWrapper
