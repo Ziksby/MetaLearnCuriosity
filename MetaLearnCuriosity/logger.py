fe1d35c0980177fd194adbae36a50a59bee1e01d
Ziksby <ZKXBAT001@myuct.ac.za>
2023-10-15 10:46:01 +0200

    style: :umbrella: code formatter

--- MetaLearnCuriosity/logger.py
+++ MetaLearnCuriosity/logger.py
@@ -5,32 +5,32 @@ import wandb
 
 class WBLogger:
     def __init__(self, config, group, tags, notes=None):
-
-        self.episode_returns = wandb.init(
-            project="MetaLearnCuriosity",
-            reinit=True,
-            config=config,
-            group=group,
-            tags=tags,
-            notes=notes,
-        )
-
-        self.losses = wandb.init(
-            project="MetaLearnCuriosity",
-            reinit=True,
-            config=config,
-            group=group,
-            tags=tags,
-            notes=notes,
-        )
+        self.config = config
+        self.tags = tags
+        self.group = group
+        self.notes = notes
 
     def log_episode_return(self, output):
+        self.episode_returns = wandb.init(
+            project="MetaLearnCuriosity",
+            config=self.config,
+            group=self.group,
+            tags=self.tags,
+            notes=self.notes,
+        )
 
         for returns in output["metrics"]["returned_episode_returns"].mean(-1).reshape(-1):
             self.episode_returns.log({"episode_return": returns})
         self.episode_returns.finish()
 
     def log_byol_losses(self, output):
+        self.losses = wandb.init(
+            project="MetaLearnCuriosity",
+            config=self.config,
+            group=self.group,
+            tags=self.tags,
+            notes=self.notes,
+        )
 
         for loss in range(len(output["rl_loss"][0].mean(-1).reshape(-1))):
             self.losses.log(
@@ -43,7 +43,13 @@ class WBLogger:
         self.losses.finish()
 
     def log_rl_losses(self, output):
-
+        self.losses = wandb.init(
+            project="MetaLearnCuriosity",
+            config=self.config,
+            group=self.group,
+            tags=self.tags,
+            notes=self.notes,
+        )
         for loss in range(len(output["rl_loss"][0].mean(-1).reshape(-1))):
             self.losses.log(
                 {
