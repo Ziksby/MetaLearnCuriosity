05ee7208910926b4f0c8238e3858a5242c6ac64f
Ziksby <batsyziki@gmail.com>
2024-12-06 08:20:11 +0000

    fix: key issues with dic

--- MetaLearnCuriosity/compile_byol_brax_fns.py
+++ MetaLearnCuriosity/compile_byol_brax_fns.py
@@ -767,9 +767,14 @@ def compile_brax_byol_fns(config):  # noqa: C901
             _update_step, runner_state, None, config["NUM_UPDATES"]
         )
         metric, _, _, _, int_lambdas = extra_info
-        rewards = metric["lifetime_episode_lengths"].mean(axis=-1)
+        rewards = metric["sum_of_rewards"].mean(axis=-1)
         rewards = rewards.reshape(-1)
         rewards = rewards[-1]
+
+        lifetime_episode_lengths = metric["lifetime_episode_lengths"].mean(axis=-1)
+        lifetime_episode_lengths = lifetime_episode_lengths.reshape(-1)
+        lifetime_episode_lengths = lifetime_episode_lengths[-1]
+
         int_lambdas = int_lambdas.mean()
         episode_returns = metric["returned_episode_returns"].mean(axis=-1)
         episode_returns = episode_returns.reshape(-1)
