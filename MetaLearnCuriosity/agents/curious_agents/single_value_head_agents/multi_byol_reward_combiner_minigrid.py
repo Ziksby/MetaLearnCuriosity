c2576e75a1205827ec4e4678e58336c6c81c240b
Ziksby <batsyziki@gmail.com>
2024-12-19 15:36:40 +0000

    feat: edot testing mini

--- MetaLearnCuriosity/agents/curious_agents/single_value_head_agents/multi_byol_reward_combiner_minigrid.py
+++ MetaLearnCuriosity/agents/curious_agents/single_value_head_agents/multi_byol_reward_combiner_minigrid.py
@@ -44,7 +44,8 @@ jax.config.update("jax_threefry_partitionable", True)
 
 environments = [
     "MiniGrid-DoorKey-5x5",
-    "MiniGrid-FourRooms" "MiniGrid-Empty-16x16",
+    "MiniGrid-FourRooms",
+    "MiniGrid-Empty-16x16",
     "MiniGrid-DoorKey-6x6",
     "MiniGrid-DoorKey-8x8",
     "MiniGrid-DoorKey-16x16",
